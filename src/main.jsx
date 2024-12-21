7102733a61955bf3c662f2d481bdf2639f9becb9
Ridwanul Islam <ridwanul161@gmail.com>
2024-12-21 16:48:24 +0300

    tostify added

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import Router from "./route/Router.jsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Router />
+      <ToastContainer />
     </BrowserRouter>
   </StrictMode>
 );
