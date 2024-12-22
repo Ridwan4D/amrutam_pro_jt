a30692716c3c53e130166b9c0d57e5280a479425
Ridwanul Islam <ridwanul161@gmail.com>
2024-12-22 17:41:59 +0300

    section title responsive for medium and large  device

--- src/component/SectinTitle/TitleWithPara.jsx
+++ src/component/SectinTitle/TitleWithPara.jsx
@@ -2,9 +2,11 @@ import PropType from "prop-types";
 
 const TitleWithPara = ({ heading, para }) => {
   return (
-    <div className="mt-20 mb-5 text-center space-y-4 max-w-3xl mx-auto px-8 md:px-0">
-      <h1 className="text-3xl md:text-5xl base_text font_dinpro_bold">{heading}</h1>
-      <p className="text-sm md:text-xl font-thin max-w-2xl mx-auto text-stone-500">
+    <div className="mt-20 md:mt-10 xl:mt-20 mb-5 text-center space-y-4 max-w-2xl xl:max-w-3xl mx-auto px-8 md:px-0">
+      <h1 className="text-3xl md:text-4xl xl:text-5xl base_text font_dinpro_bold">
+        {heading}
+      </h1>
+      <p className="text-sm md:text-lg xl:text-xl font-thin max-w-2xl mx-auto text-stone-500">
         {para}
       </p>
     </div>
