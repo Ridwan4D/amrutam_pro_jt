20846647af2fb0c54aa235fcb7817b606e7d360a
Ridwanul Islam <ridwanul161@gmail.com>
2024-12-22 17:19:52 +0300

    base color setup

---
+++ src/component/DicoverAyurveda/DiscoverAyurveda.jsx
@@ -0,0 +1,10 @@
+
+const DiscoverAyurveda = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DiscoverAyurveda;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import Banner from "../../component/Banner/Banner";
+import DiscoverAyurveda from "../../component/DicoverAyurveda/DiscoverAyurveda";
 
 const Home = () => {
   return (
     <div>
+      {/* banner section */}
       <Banner />
+      {/* Discover Ayurveda section */}
+      <DiscoverAyurveda />
     </div>
   );
 };
