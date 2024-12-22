bc609afc06f5294eda0efee4b936df6452e0ee9a
Ridwanul Islam <ridwanul161@gmail.com>
2024-12-22 17:43:21 +0300

    section title responsiveness updated for all device

--- src/component/SectinTitle/TitleWithPara.jsx
+++ src/component/SectinTitle/TitleWithPara.jsx
@@ -2,7 +2,7 @@ import PropType from "prop-types";
 
 const TitleWithPara = ({ heading, para }) => {
   return (
-    <div className="mt-20 md:mt-10 xl:mt-20 mb-5 text-center space-y-4 max-w-2xl xl:max-w-3xl mx-auto px-8 md:px-0">
+    <div className="mt-10 xl:mt-20 mb-5 text-center space-y-4 max-w-2xl xl:max-w-3xl mx-auto px-8 md:px-0">
       <h1 className="text-3xl md:text-4xl xl:text-5xl base_text font_dinpro_bold">
         {heading}
       </h1>
