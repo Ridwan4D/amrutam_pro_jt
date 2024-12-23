9de50fcd9b0cce9a01a97b981373a44c7d77b6a6
Ridwanul Islam <ridwanul161@gmail.com>
2024-12-23 19:25:53 +0300

    approach seciton card is designed as figma

--- src/component/ApproachSection/ApproachSection.jsx
+++ src/component/ApproachSection/ApproachSection.jsx
@@ -30,7 +30,7 @@ const ApproachSection = () => {
   ];
 
   return (
-    <div>
+    <div className="px-2 md:px-0">
       {/* Section Title */}
       <SectionTitle
         heading={"Our Ayurvedic Approach"}
@@ -53,11 +53,14 @@ const ApproachSection = () => {
       >
         {approachData.map((item, idx) => (
           <SwiperSlide key={idx}>
-            <div className="p-4 border rounded-lg shadow-md bg-white">
-              <h3 className="text-xl font-bold mb-2">
-                Step {item.step}: {item.title}
+            <div className="p-5 border-t-4 border-green-800 rounded-xl shadow-md bg-yellow-50 flex flex-col items-center space-y-4 text-center min-h-64">
+              <div className="w-16 h-16 bg-green-900/60 rounded-full flex items-center justify-center text-5xl text-white font_dinpro_bold">
+                <h3>{item.step}</h3>
+              </div>
+              <h3 className="text-2xl font-bold mb-2 font_dinpro_bold base_text">
+                {item.title}
               </h3>
-              <p className="text-gray-600">{item.desc}</p>
+              <p className="text-zinc-800 text-xl">{item.desc}</p>
             </div>
           </SwiperSlide>
         ))}
